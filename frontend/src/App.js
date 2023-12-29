0996b46eb5d0067d4f84d300a6e01f05177a4d2e
Fabio Franceschetto <fabiof.frances@gmail.com>
2023-12-29 09:00:29 -0300

    Aleração no arquivo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <p>I was changed two!</p>
+        <p>I was changed three!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
